abc0f890379c5b81145612d32b56cca4c055a12c
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2017-09-18 11:48:27 -0400

    Fix columns names and order for the returned dataframe

--- src/gi/overrides/Mirbooking.py
+++ src/gi/overrides/Mirbooking.py
@@ -11,5 +11,5 @@ class Broker(Mirbooking.Broker):
             for target_site in self.get_target_sites():
                 for occupant in target_site.occupants:
                     yield target_site.target.get_accession(), target_site.target.get_name(), occupant.mirna.get_accession(), occupant.mirna.get_name(), target_site.position, self.get_score_table().compute_score(occupant.mirna, target_site.target, target_site.position), occupant.quantity, self.get_target_site_vacancy(target_site)
-        columns = ['target', 'target_name', 'mirna', 'mirna_name', 'position', 'probability', 'occupancy', 'vacancy']
-        return pd.DataFrame(get_target_sites(), columns=columns).set_index(['target', 'mirna', 'position'])
+        columns = ['target_accession', 'target_name', 'target_quantity', 'position', 'vacancy', 'mirna_accession', 'mirna_name', 'mirna_quantity', 'probability', 'occupancy']
+        return pd.DataFrame(get_target_sites(), columns=columns).set_index(['target_accession', 'mirna_accession', 'position'])
