b916671a30e87762dbbbf63da0ca066ec03630d3
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2019-02-16 00:02:36 -0500

    Fix more warnings with -Dwarning_level=2

--- src/mirbooking-broker.c
+++ src/mirbooking-broker.c
@@ -162,6 +162,7 @@ mirbooking_broker_get_property (GObject *object, guint property_id, GValue *valu
             break;
         case PROP_SCORE_TABLE:
             g_value_set_object (value, self->priv->score_table);
+            break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
             break;
