a0e15fe1847cf222ccaab008ac507f2203b14662
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2019-09-13 12:50:20 -0400

    sparse: Add missing sparse-mkl-lapack.c

---
+++ contrib/sparse/sparse-mkl-lapack.c
@@ -0,0 +1,66 @@
+#include "sparse.h"
+#include "sparse-private.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <mkl_lapacke.h>
+#include <assert.h>
+
+void
+sparse_mkl_lapack_init (SparseSolver *solver)
+{
+
+}
+
+void
+sparse_mkl_lapack_clear (SparseSolver *solver)
+{
+
+}
+
+int
+sparse_mkl_lapack_solve (SparseSolver *solver,
+                      SparseMatrix *A,
+                      void         *x,
+                      const void   *b)
+{
+    lapack_int ret;
+
+    assert (A->storage == SPARSE_MATRIX_STORAGE_DENSE);
+    assert (A->shape[0] == A->shape[1]);
+
+    lapack_int *pivot = malloc (A->shape[0] * sizeof (lapack_int));
+
+    if (A->type == SPARSE_MATRIX_TYPE_DOUBLE)
+    {
+        memcpy (x, b, A->shape[1] * sizeof (double));
+        ret = LAPACKE_dgesv (LAPACK_ROW_MAJOR,
+                             A->shape[0],
+                             1,
+                             A->data,
+                             A->shape[1],
+                             pivot,
+                             (double*) x,
+                             1);
+    }
+    else if (A->type == SPARSE_MATRIX_TYPE_FLOAT)
+    {
+        memcpy (x, b, A->shape[1] * sizeof (float));
+        ret = LAPACKE_sgesv (LAPACK_ROW_MAJOR,
+                             A->shape[0],
+                             1,
+                             A->data,
+                             A->shape[1],
+                             pivot,
+                             (float*) x,
+                             1);
+    }
+    else
+    {
+        assert (0);
+    }
+
+    free (pivot);
+
+    return ret == 0;
+}
